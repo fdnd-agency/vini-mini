32460e980d16d3bc83305b5a83d2c5b18165dfde
Koop Reynders <k.g.reynders@hva.nl>
2023-03-07 12:08:16 +0100

    added categorie query

--- src/routes/api/v1/categorie/+server.js
+++ src/routes/api/v1/categorie/+server.js
@@ -1,1 +1,44 @@
-//categorie
+//Toon categorieen
+
+import { GraphQLClient, gql } from 'graphql-request'
+import { HYGRAPH_KEY, HYGRAPH_URL_HIGH_PERFORMANCE } from '$env/static/private'
+
+import { responseInit } from '$lib/server/responseInit'
+
+const hygraph = new GraphQLClient(HYGRAPH_URL_HIGH_PERFORMANCE, {
+  headers: {
+    Authorization: `Bearer ${HYGRAPH_KEY}`,
+  },
+})
+
+export async function GET({ url }) {
+  let first = Number(url.searchParams.get('first') ?? 5)
+  let skip = Number(url.searchParams.get('skip') ?? 0)
+  let direction = url.searchParams.get('direction') === 'ASC' ? 'ASC' : 'DESC'
+  let orderBy = (url.searchParams.get('orderBy') ?? 'publishedAt') + '_' + direction
+
+  const query = gql`
+    query getCategorie($first: Int, $skip: Int, $orderBy: ProductOrderByInput) {
+      categorie(first: $first, skip: $skip, orderBy: $orderBy) {
+        id
+        titel
+        beschrijving {
+          html
+        }
+        producten {
+          titel
+          slug
+        }
+      }
+      productenConnection {
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+          pageSize
+        }
+      }
+    }
+  `
+  const data = await hygraph.request(query, { first, skip, orderBy })
+  return new Response(JSON.stringify(data), responseInit)
+}
