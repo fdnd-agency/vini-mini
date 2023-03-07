77709aae748d7d30afebddbcea6006b5213a58cd
Koop Reynders <k.g.reynders@hva.nl>
2023-03-07 16:53:10 +0100

    Update +server.js

--- src/routes/api/v1/categorie/+server.js
+++ src/routes/api/v1/categorie/+server.js
@@ -1,16 +1,3 @@
-//Toon categorieen
-
-import { GraphQLClient, gql } from 'graphql-request'
-import { HYGRAPH_KEY, HYGRAPH_URL_HIGH_PERFORMANCE } from '$env/static/private'
-
-import { responseInit } from '$lib/server/responseInit'
-
-const hygraph = new GraphQLClient(HYGRAPH_URL_HIGH_PERFORMANCE, {
-  headers: {
-    Authorization: `Bearer ${HYGRAPH_KEY}`,
-  },
-})
-
 export async function GET({ url }) {
   let first = Number(url.searchParams.get('first') ?? 10)
   let skip = Number(url.searchParams.get('skip') ?? 0)
@@ -19,10 +6,44 @@ export async function GET({ url }) {
 
   const query = gql`
     query getProducten($first: Int, $skip: Int, $orderBy: ProductOrderByInput) {
-      categorie(first: $first, skip: $skip, orderBy: $orderBy) {
+      producten(first: $first, skip: $skip, orderBy: $orderBy) {
         titel
+        slug
+        tagline
+        prijs
+        categorie {
+          titel
+          beschrijving {
+            html
+          }
+          plaatjes {
+            url
+          }
+        }
         beschrijving {
           html
+        }
+        plaatjes {
+          url
+        }
+        schema {
+          titel
+          beschrijving {
+            html
+          }
+          stappen {
+            titel
+            beschrijving {
+              html
+            }
+            gap
+          }
+          plaatjes {
+            url
+          }
+        }
+        tips {
+          tekst
         }
       }
       productenConnection {
