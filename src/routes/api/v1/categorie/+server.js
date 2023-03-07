e11ba5a38e288e41e130ab33bfc743dd3e75ea8f
Koop Reynders <k.g.reynders@hva.nl>
2023-03-07 16:40:55 +0100

    Update +server.js

--- src/routes/api/v1/categorie/+server.js
+++ src/routes/api/v1/categorie/+server.js
@@ -20,15 +20,15 @@ export async function GET({ url }) {
   const query = gql`
     query getCategorie($first: Int, $skip: Int, $orderBy: ProductOrderByInput) {
       categorie(first: $first, skip: $skip, orderBy: $orderBy) {
-        id
+//        id
         titel
         beschrijving {
           html
         }
-        producten {
-          titel
-          slug
-        }
+//        producten {
+//          titel
+//          slug
+//        }
       }
       productenConnection {
         pageInfo {
